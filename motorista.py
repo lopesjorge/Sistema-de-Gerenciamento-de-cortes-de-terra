67414f356ae788a1b4cbae074f5eec7446d42849
Jorge Lopes <53401427+lopesjorge@users.noreply.github.com>
2021-08-30 09:28:05 -0300

    Add files via upload

---
+++ motorista.py
@@ -0,0 +1,33 @@
+class Motorista:
+    def __init__(self, placa, matricula_motorista,cnh,data_liberacao):
+        self.placa = placa
+        self.matricula_motorista = matricula_motorista
+        self.cnh = cnh
+        self.data_liberacao = data_liberacao
+        
+        def setPlaca (self,placa):
+            self.placa = placa
+            
+        def setMatriculaMotorista (self, matricula_motorista):
+            self.matricula_motorista = matricula_motorista
+            
+        def setCnh (self, cnh):
+            self.cnh = cnh
+        
+        def setDataLiberacao (self,data_liberacao):
+            self.data_liberacao = data_liberacao
+            
+        def getPlaca (self):
+            self.placa = placa
+            
+        def getMatriculaMotorista (self):
+            self.matricula_motorista = matricula_motorista
+            
+        def getCnh (self):
+            self.chn = cnh
+            
+        def getDataLiberacao (self):
+            self.data_liberacao = data_liberacao
+            
+            
+        
