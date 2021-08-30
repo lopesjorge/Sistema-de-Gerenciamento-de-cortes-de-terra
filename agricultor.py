a41e5aae76aed71abc6f1663c31d8a8f105aa8e6
Jorge Lopes <53401427+lopesjorge@users.noreply.github.com>
2021-08-30 11:01:42 -0300

    Add files via upload

---
+++ agricultor.py
@@ -0,0 +1,16 @@
+class Agricultor:
+    def __init__(self,nome,data_nascimento,cpf,endereco):
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        self.cpf = cpf
+        self.endereco
+        
+        def setNome (self,nome):
+            self.nome = nome
+            
+        def setDataNascimento (self, data_nascimento):
+            self.data_nascimento = data_nascimento
+        
+        def setCpf (self,cpf):
+            self.cpf = cpf
+        
